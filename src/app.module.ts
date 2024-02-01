4c4191b8b2d6b52f8c24dadf9bd5590264ec69c6
RattanakpanhaKONG <krattanakpanha@gmail.com>
2024-02-01 16:22:46 +0700

    Implementing passport-local strategy (username/password login)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
+import { LocalStrategy } from './auth/local.strategy';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, PassportModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService, LocalStrategy],
 })
 export class AppModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  password: string;
+};
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: 1,
+      name: 'John Doe',
+      username: 'john_doe',
+      password: 'password1',
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      username: 'jane_smith',
+      password: 'password2',
+    },
+    {
+      id: 3,
+      name: 'Bob Johnson',
+      username: 'bob_johnson',
+      password: 'password3',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username)
+  }
+}
